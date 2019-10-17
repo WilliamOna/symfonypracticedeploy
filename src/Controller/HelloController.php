9e92b4ebc989985a5b7de90ee1862d3c20227c56
wjona13 <william.ona@revature.com>
2019-10-16 22:26:58 -0500

    added hello controller and lucky number controller

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,31 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController
+{
+      /**
+      * @Route("/lucky/number")
+      */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+          /**
+      * @Route("/")
+      */
+      public function hello()
+      {
+  
+          return new Response(
+              '<html><body>Symfony app v1</body></html>'
+          );
+      }
+}
